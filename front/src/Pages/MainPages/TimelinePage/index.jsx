1d1931aaa1f88fdd4fcba1e802d3128882fecd8d
isabellexvr <isabellexavier59@gmail.com>
2023-03-29 23:59:15 -0300

    style: timeline selector

--- front/src/Pages/MainPages/TimelinePage/index.jsx
+++ front/src/Pages/MainPages/TimelinePage/index.jsx
@@ -6,13 +6,17 @@ import Footer from "../Constants/Footer";
 import { themes } from "../../../Contexts/ThemeContext";
 import { colors } from "../../../Services/Constants/colors";
 import Post from "./Components/Post";
+import { useState } from "react";
 
 const postsMocked = [1, 2, 3, 4, 5];
 
 const user = {};
 
+const TIMELINESTYPES = ["My Timeline", "Communities"];
+
 export default function TimelinePage() {
   const { theme, setTheme } = useTheme();
+  const [selectedTimeline, setSelectedTimeline] = useState(0);
   console.log(theme);
   return (
     <>
@@ -22,13 +26,23 @@ export default function TimelinePage() {
         <FirstSection>
           <CommunitiesContainer>
             {postsMocked.map((c) => (
-              <Community communityPic={"https://www.chess.com/bundles/web/images/offline-play/standardboard.1d6f9426.png"}>
-                <CommunityFooter></CommunityFooter>
+              <Community
+                communityPic={
+                  "https://www.chess.com/bundles/web/images/offline-play/standardboard.1d6f9426.png"
+                }
+              >
+                <CommunityFooter>chess</CommunityFooter>
               </Community>
             ))}
           </CommunitiesContainer>
         </FirstSection>
-
+        <TimelineSelection>
+          {TIMELINESTYPES.map((t, i) => (
+            <TimelineButton isSelected={selectedTimeline === i} onClick={() => setSelectedTimeline(i)}>
+              {t}
+            </TimelineButton>
+          ))}
+        </TimelineSelection>
         {postsMocked.map((p) => (
           <Post></Post>
         ))}
@@ -38,28 +52,49 @@ export default function TimelinePage() {
   );
 }
 
+const TimelineButton = styled.button`
+  all: unset;
+  width: 50%;
+  height: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${p => p.isSelected ? colors.orange : "none"};
+  border-radius: 50px;
+  font-size: 17px;
+`;
+
+const TimelineSelection = styled.div`
+  width: 82%;
+  height: 40px;
+  background-color: ${colors.lighterBlack};
+  border-radius: 50px;
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
 const FirstSectionTitle = styled.h1`
-      font-size: 22px;
-    margin-bottom: 10px;
-    width: 82%;
-`
+  font-size: 22px;
+  margin-bottom: 10px;
+  width: 82%;
+`;
 
 const FirstSection = styled.section`
-  height: 200px;
+  height: fit-content;
   width: 82%;
   overflow-x: scroll;
-  >h1:first-child{
-
+  > h1:first-child {
   }
 `;
 
 const CommunitiesContainer = styled.div`
   display: flex;
-width: fit-content;
+  width: fit-content;
 `;
 
 const Community = styled.div`
-  background-image: url(${p => p.communityPic});
+  background-image: url(${(p) => p.communityPic});
   background-size: contain;
   background-repeat: no-repeat;
   height: 160px;
@@ -73,7 +108,7 @@ const Community = styled.div`
 
 const CommunityFooter = styled.div`
   width: 100%;
-  height: 45px;
+  height: 47px;
   background-color: ${colors.lighterBlack};
   -webkit-mask-image: radial-gradient(
     circle at top,
@@ -81,8 +116,9 @@ const CommunityFooter = styled.div`
     black 5px
   );
   border-bottom-right-radius: 15px;
-border-bottom-left-radius: 15px;
+  border-bottom-left-radius: 15px;
   position: absolute;
   bottom: 0;
-
-`
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+`;
