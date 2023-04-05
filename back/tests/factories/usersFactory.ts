1bc7abf3119feeafa54f7e4807587aab1d5fc8f8
isabellexvr <isabellexavier59@gmail.com>
2023-04-05 20:29:12 -0300

    feat: install faker + user factorie and tests

---
+++ back/tests/factories/usersFactory.ts
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt';
+import { faker } from '@faker-js/faker';
+import { db } from '../../src/config/db';
+import { users } from '@prisma/client';
+
+export async function createFakeUser(body: Partial<users>) {
+    const password = body.password || faker.internet.password(6);
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    return db.prisma.users.create({
+        data: {
+            fullName: body.fullName || faker.name.fullName(),
+            userName: body.userName || faker.name.firstName(),
+            picture: body.picture || faker.image.avatar(),
+            email: body.email || faker.internet.email(),
+            password: hashedPassword
+        }
+    });
+};
--- back/tests/integration/users.test.ts
+++ back/tests/integration/users.test.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import app from "../../src/server";
 import supertest from "supertest";
 import { cleanDb, disconnectDb } from "../helpers";
@@ -8,7 +9,7 @@ beforeAll(async () => {
     cleanDb();
 })
 
-afterAll(async ( ) => {
+afterAll(async () => {
     disconnectDb();
     process.exit(1)
 })
@@ -17,5 +18,29 @@ describe("POST /users/sign-up", () => {
     it("Should respond with status 400 if body is not given", async () => {
         const res = await server.post("/users/sign-up");
         expect(res.statusCode).toBe(400);
+    });
+
+    it("Should respond with status 400 if a invalid body is sent", async () => {
+
+        const body = { [faker.lorem.word()]: faker.lorem.word() };
+
+        const res = await server.post("/users/sign-up").send(body);
+
+        expect(res.statusCode).toBe(404);
+    });
+
+    describe("Valid Body Tests", () => {
+        const validBodyGenerator = () => ({
+            fullName: faker.name.fullName(),
+            userName: faker.name.firstName(),
+            picture: faker.image.avatar(),
+            email: faker.internet.email(),
+            password: faker.internet.password(6)
+        })
+    });
+
+    it("should respond with status 409 if username already exists", async () => {
+        
     })
-})
+
+});
