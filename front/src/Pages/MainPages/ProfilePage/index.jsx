ae1fb01a4043d8b8527ba503aa50d22e89a1566d
isabellexvr <isabellexavier59@gmail.com>
2023-04-08 18:09:25 -0300

    feat: statistics + loading

--- front/src/Pages/MainPages/ProfilePage/index.jsx
+++ front/src/Pages/MainPages/ProfilePage/index.jsx
@@ -10,11 +10,16 @@ import { SlOptions } from "react-icons/sl";
 import { colors } from "../../Assets/colors";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import LoadingPosts from "../TimelinePage/Components/LoadingPosts";
+
+const PROFILEVIEWS = ["Posts", "Communities"];
 
 export default function ProfilePage({ isModalOpened, setIsModalOpened }) {
   const { theme, setTheme } = useTheme();
   const { userInfo, setUserInfo } = useUserInfo();
   const [loading, setLoading] = useState(false);
+  const [view, setView] = useState(0);
+  const [usersPosts, setUsersPosts] = useState([]);
 
   const navigate = useNavigate();
 
@@ -27,14 +32,13 @@ export default function ProfilePage({ isModalOpened, setIsModalOpened }) {
     } else {
       const info = JSON.parse(isLoggedIn);
       setUserInfo(info);
-      setLoading(true);
     }
   }, []);
 
   return (
     <>
       {userInfo && (
-        <ProfileBackground theme={theme}> 
+        <ProfileBackground theme={theme}>
           <ProfileHeader>
             <button>
               <HiArrowNarrowLeft onClick={() => navigate("/timeline")} />
@@ -48,11 +52,33 @@ export default function ProfilePage({ isModalOpened, setIsModalOpened }) {
             <UserProfilePic src={userInfo.picture} />
             <h1>{userInfo.fullName}</h1>
             <h2>@{userInfo.userName}</h2>
-            <UserBiography theme={theme}><strong>-</strong> user's bio</UserBiography>
+            <UserBiography theme={theme}>
+              <strong>-</strong> user's bio
+            </UserBiography>
           </UserMainInfoContainer>
           <UserStatisticsContainer>
-
+            <Statistic theme={theme}>
+              <h1>100</h1>
+              <h4>Posts</h4>
+            </Statistic>
+            <Statistic>
+              <h1>1,2k</h1>
+              <h4>Followers</h4>
+            </Statistic>
+            <Statistic>
+              <h1>100</h1>
+              <h4>Following</h4>
+            </Statistic>
           </UserStatisticsContainer>
+          <ViewSelection>
+            {PROFILEVIEWS.map((v, i) => (
+              <ViewButton isSelected={view === i} onClick={() => setView(i)}>
+                {v}
+              </ViewButton>
+            ))}
+          </ViewSelection>
+          {loading && <LoadingPosts theme={theme} />}
+
           <Footer theme={theme} setIsModalOpened={setIsModalOpened} />
           <PostModal
             isModalOpened={isModalOpened}
@@ -69,8 +95,8 @@ export default function ProfilePage({ isModalOpened, setIsModalOpened }) {
 }
 
 const ProfileBackground = styled.div`
-    background-color: ${(p) => p.theme.backgroundColor};
-    z-index: 0;
+  background-color: ${(p) => p.theme.backgroundColor};
+  z-index: 0;
   height: fit-content;
   padding-bottom: 120px;
   color: ${(p) => p.theme.fontColor};
@@ -78,8 +104,7 @@ const ProfileBackground = styled.div`
   flex-direction: column;
   align-items: center;
   position: relative;
-
-`
+`;
 
 const ProfileHeader = styled.div`
   width: 85%;
@@ -159,10 +184,10 @@ const UserProfilePic = styled.img`
 `;
 
 const UserBiography = styled.h3`
-color: ${p => p.theme.fontColor};
->strong{
-  color: ${colors.orange};
-}
+  color: ${(p) => p.theme.fontColor};
+  > strong {
+    color: ${colors.orange};
+  }
 `;
 
 const UserStatisticsContainer = styled.div`
@@ -172,5 +197,48 @@ const UserStatisticsContainer = styled.div`
   border-radius: 15px;
   border: 1px solid grey;
   background-color: ${colors.lighterBlack};
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 10px;
+`;
 
-`
+const Statistic = styled.div`
+  height: 100%;
+  width: 80px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  > h1 {
+    font-size: 20px;
+    color: ${(p) => p.theme.fontColor};
+    margin-bottom: 5px;
+  }
+  > h4 {
+    font-size: 14px;
+    opacity: 0.7;
+    color: ${(p) => p.theme.fontColor};
+  }
+`;
+
+export const ViewButton = styled.button`
+  all: unset;
+  width: 50%;
+  border: 4px solid ${colors.lighterBlack};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(p) => (p.isSelected ? colors.orange : "none")};
+  border-radius: 50px;
+  font-size: 17px;
+`;
+
+export const ViewSelection = styled.div`
+  width: 82%;
+  height: 45px;
+  background-color: ${colors.lighterBlack};
+  border-radius: 50px;
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
