901de498a77269adb3626bfb8719626951c00829
isabellexvr <isabellexavier59@gmail.com>
2023-04-10 08:56:55 -0300

    feat: likes and comments controllers

--- back/src/controllers/commentsControllers.ts
+++ back/src/controllers/commentsControllers.ts
@@ -14,7 +14,31 @@ export async function postComment(req: AuthenticatedRequest, res: Response) {
 
     try {
         await commentsServices.postComment({ ...comment, ownerId: userId });
-        res.sendStatus(200);
+        res.sendStatus(201);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
+
+export async function editComment(req: AuthenticatedRequest, res: Response) {
+    const userId = req.userId;
+    const comment: PartNewComment = req.body.postId;
+
+    try {
+        await commentsServices.editComment({ ...comment, ownerId: userId });
+        res.sendStatus(200)
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
+
+export async function deleteComment(req: AuthenticatedRequest, res: Response) {
+    const userId = req.userId;
+    const comment: PartNewComment = req.body.postId;
+
+    try {
+        await commentsServices.deleteComment({ ...comment, ownerId: userId });
+        res.sendStatus(200)
     } catch (error) {
         return res.status(500).send(error);
     }
--- back/src/controllers/likesControllers.ts
+++ back/src/controllers/likesControllers.ts
@@ -9,9 +9,20 @@ export async function postLike(req: AuthenticatedRequest, res: Response) {
     try {
         const data = { ownerId: userId, postId };
         await likesServices.postLike(data);
-        res.sendStatus(200);
+        res.sendStatus(201);
     } catch (error) {
         return res.status(500).send(error);
     }
 }
 
+export async function dislike(req: AuthenticatedRequest, res: Response) {
+    const userId = req.userId;
+    const postId: number = req.body.postId;
+    try {
+        const data = { ownerId: userId, postId };
+        await likesServices.deleteLike(data);
+        res.sendStatus(200);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
