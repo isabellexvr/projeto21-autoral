035bc9c328e823808e024adefe8d59d361e6b89d
isabellexvr <isabellexavier59@gmail.com>
2023-04-16 15:50:57 -0300

    fix: users and communities schemas

--- back/src/schemas/communitiesSchemas.ts
+++ back/src/schemas/communitiesSchemas.ts
@@ -1,10 +1,19 @@
 import joi from "joi";
 
 export const newCommunitySchema = joi.object({
-    name: joi.string().required(),
-    description: joi.string(),
-    icon: joi.string().allow(null),
-    cover: joi.string().allow(null),
-    categoryId: joi.number().required(),
-    createdAt: joi.date()
+    communityInfo: {
+        name: joi.string().required(),
+        description: joi.string(),
+        icon: joi.string().allow(null),
+        cover: joi.string().allow(null),
+        categoryId: joi.number().required(),
+        createdAt: joi.date().optional()
+    },
+    locationInfo: joi.object({
+        country: joi.string().required(),
+        countryIso2: joi.string().max(2).required(),
+        state: joi.string().required(),
+        stateIso2: joi.string().max(2).required(),
+        city: joi.string().required()
+    })
 });
--- back/src/schemas/signupSchema.ts
+++ back/src/schemas/signupSchema.ts
@@ -1,9 +1,19 @@
 import joi from "joi";
 
 export const signupSchema = joi.object({
-    fullName: joi.string().max(30).required(),
-    userName: joi.string().max(15).required(),
-    picture: joi.optional(),
-    email: joi.string().email().required(),
-    password: joi.string().required(),
+    userInfo: joi.object({
+        fullName: joi.string().max(30).required(),
+        userName: joi.string().max(15).required(),
+        picture: joi.string().optional(),
+        cover: joi.string().optional(),
+        email: joi.string().email().required(),
+        password: joi.string().required(),
+    }),
+    locationInfo: joi.object({
+        country: joi.string().required(),
+        countryIso2: joi.string().max(2).required(),
+        state: joi.string().required(),
+        stateIso2: joi.string().max(2).required(),
+        city: joi.string().required()
+    })
 });
